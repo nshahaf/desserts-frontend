4d3e00b90322345f3b24cbfd3d5fd62b2c6e585d
shahaf noked <shahafnoked@gmail.com>
2024-08-28 19:38:07 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ComponentTemplate from "./components/CompTemplate.vue";
+
+</script>
+
+<template>
+  <h1>Hello world</h1>
+  <ComponentTemplate/>
+ 
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/CompTemplate.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+  </section>
+</template>
+
+
+<style lang="scss" scoped>
+section{
+  
+}
+
+</style>
